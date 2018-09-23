4de101108bd963feb265911bb415c07830002197
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-09-23 12:37:42 -0500

    Splash animated v1

--- app/src/main/java/com/example/ricardo/pruebatecnicaareamovil/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/ricardo/pruebatecnicaareamovil/ui/splash/SplashActivity.kt
@@ -3,8 +3,11 @@ package com.example.ricardo.pruebatecnicaareamovil.ui.splash
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
 import com.example.ricardo.pruebatecnicaareamovil.R
 import com.example.ricardo.pruebatecnicaareamovil.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -14,10 +17,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out)
+        splashImgView.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
 
         Handler().postDelayed({
-            startActivity<MainActivity>()
-            finish()
+            splashImgView.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+            Handler().postDelayed({
+                splashImgView.visibility = View.GONE
+                startActivity<MainActivity>()
+                finish()
+            },500)
         }, timer.toLong())
 
     }
